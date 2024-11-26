406997b0dd4da32a7ced3c4192e0c1a60b17d15e
Syed <dcsyedfaraz@gmail.com>
2024-11-26 23:37:16 +0500

    "Added primeicons dependency and updated app.js to import primeicons and configure PrimeVue with Aura theme"

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,13 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from 'primevue/config';
-
+import { Link } from '@inertiajs/vue3';
+import Aura from '@primevue/themes/aura';
+import { Head } from '@inertiajs/vue3';
+import ToastService from 'primevue/toastservice';
+// import store from './store/index';
+import 'primeicons/primeicons.css';
+import ConfirmationService from 'primevue/confirmationservice';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -21,7 +27,18 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(PrimeVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                    options: {
+                        darkModeSelector: 'off'
+                    }
+                }
+            })
+            .use(ToastService)
+            .use(ConfirmationService)
+            .component('Link', Link)
+            .component('Head', Head)
             .mount(el);
     },
     progress: {
